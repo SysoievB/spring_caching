d960b15d6562a070a120a5af546dffbf916b695b
SysoievB <sysoiev.bogdan.ua@gmail.com>
2023-07-23 14:26:20 +0200

    Added CRUD REST

--- src/main/java/com/caching/repository/BookRepo.java
+++ src/main/java/com/caching/repository/BookRepo.java
@@ -3,16 +3,5 @@ package com.caching.repository;
 import com.caching.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface BookRepo extends JpaRepository<Book, Long> {
-/*
-    Optional<Book> findById(Long id);
-
-    List<Book> findAll();
-
-    Book save(Book book);
-
-    void deleteById(Long id);*/
 }
